b1c95486b96e6baefaae25938e283f81dc5b43d0
Aaron Wilson <aaron@unionstudios.com>
2022-01-19 13:42:36 -0500

    adding in the server and api questions

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,5 +1,46 @@
 <template>
-    <main class="faq">
-        <h1>Frequesntly Asked Questions</h1>
-    </main>
+  <main class="faq">
+    <h1>Frequesntly Asked Questions</h1>
+
+    <div class="error" v-if="error">Can't Load the questions</div>
+
+    <section class="list">
+      <article v-for="question of questions" :key="question._id">
+        <h2 v-html="question.title"></h2>
+        <p v-html="question.content"></p>
+      </article>
+    </section>
+  </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      questions: [],
+      error: null,
+    };
+  },
+  created() {
+    fetch('http://localhost:3000/questions')
+      .then((response) => {
+        if (response.ok) {
+          // Returns a new promise
+          return response.json();
+        } else {
+          return Promise.reject("error");
+        }
+      })
+      .then((result) => {
+        // Success
+        console.log("JSON:", result);
+        this.questions = result
+      })
+      .catch((e) => {
+        // Failure
+        console.error(e);
+        this.error = e
+      });
+  },
+};
+</script>
--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -4,3 +4,11 @@
         <router-link :to="{ name: 'faq' }">FAQ</router-link>
     </nav>
 </template>
+
+<style lang="stylus" scoped>
+@import '../style/imports';
+
+.router-link-exact-active {
+    border-bottom-color: $primary-color;
+}
+</style>
