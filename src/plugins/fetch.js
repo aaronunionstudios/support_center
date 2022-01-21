ae1fa75a02497ddc95680dd0165efd8355962a85
Aaron Wilson <aaron@unionstudios.com>
2022-01-21 14:53:51 -0500

    adding in fetch plugin

--- src/plugins/fetch.js
+++ src/plugins/fetch.js
@@ -1,5 +1,22 @@
-export default {
-    install (Vue) {
-        console.log('Installed.... Boy!')
+let baseUrl
+
+export async function $fetch (url) {
+    const response = await fetch(`${baseUrl}${url}`)
+    if (response.ok) {
+        const data = await response.json()
+        return data
+    } else {
+        const error = new Error('error')
+        throw error
     }
+}
+
+export default {
+    install (Vue, options) {
+        console.log('Installed', options)
+        //plugin options
+        baseUrl = options.baseUrl
+        //fetch
+        Vue.prototype.$fetch = $fetch
+    },
 }
