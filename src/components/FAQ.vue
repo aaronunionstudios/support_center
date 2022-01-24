7c8444b0ca39228b3009efbfdd30c894b618e226
Aaron Wilson <aaron@unionstudios.com>
2022-01-24 09:53:11 -0500

    building out mixin

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -6,7 +6,7 @@
     <div class="error" v-if="error">Can't Load the questions</div>
 
     <section class="list">
-      <article v-for="question of questions" :key="question._id">
+      <article v-for="question of questionList" :key="question._id">
         <h2 v-html="question.title"></h2>
         <p v-html="question.content"></p>
       </article>
@@ -15,23 +15,29 @@
 </template>
 
 <script>
+import RemoteData from '../mixins/remoteData'
 export default {
-    data() {
-        return {
-            questions: [],
-            error: null,
-            loading: false,
-        };
-    },
-    async created () {
-        this.loading = true
-        try {
-          this.questions = await this.$fetch('questions')
-        } catch (e) {
-          this.error = e
-        }
-        this.loading = false
-    },
+  mixins: [
+    RemoteData({
+      questionList: 'questions',
+    }),
+  ],
+  data() {
+      return {
+          questions: [],
+          error: null,
+          loading: false,
+      };
+  },
+  async created () {
+      this.loading = true
+      try {
+        this.questions = await this.$fetch('questions')
+      } catch (e) {
+        this.error = e
+      }
+      this.loading = false
+  },
 //   created() {
 //     fetch('http://localhost:3000/questions')
 //       .then((response) => {
